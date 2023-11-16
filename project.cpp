28378e64dd09c684542975a58f46922d8224f679
ricardoxu6 <xutianch@usc.edu>
2023-11-16 16:13:56 -0500

    Generate ICFG

---
+++ project.cpp
@@ -0,0 +1,53 @@
+
+//===- SVF-Project -------------------------------------//
+//
+//                     SVF: Static Value-Flow Analysis ss
+//
+// Copyright (C) <2013->  <Yulei Sui>
+//
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//===-----------------------------------------------------------------------===//
+#include "SVF-LLVM/LLVMUtil.h"
+
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace llvm;
+using namespace std;
+using namespace SVF;
+
+static llvm::cl::opt<std::string> InputFilename(cl::Positional,
+        llvm::cl::desc("<input bitcode>"), llvm::cl::init("-"));
+
+int main(int argc, char ** argv) {
+
+    int arg_num = 0;
+    char **arg_value = new char*[argc];
+    std::vector<std::string> moduleNameVec;
+    SVF::LLVMUtil::processArguments(argc, argv, arg_num, arg_value, moduleNameVec);
+    cl::ParseCommandLineOptions(arg_num, arg_value,
+                                "Whole Program Points-to Analysis\n");
+
+    SVFModule* svfModule = LLVMModuleSet::getLLVMModuleSet()->buildSVFModule(moduleNameVec);
+    // Build Program Assignment Graph (SVFIR)
+    SVFIRBuilder builder;
+    SVFIR* pag = builder.build(svfModule);
+    /// ICFG
+    ICFG* icfg = pag->getICFG();
+
+    return 0;
+}
